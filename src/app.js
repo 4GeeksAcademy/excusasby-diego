657474cdef08ef125ab300f17d6f8794a847593f
DIEGO <diegoovalle15@gmail.com>
2024-07-01 19:22:47 +0000

    excusas by diegoO

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello rigo from console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = [" dog", " grandma", " cat", " neighbor", ""];
+  let action = [" ate", " lost", " broke", " hid"];
+  let possession = [" my homework", " my keys", " my car", " my shoes"];
+  let where = [" in the park", " at home", " in my car", " at school"];
+
+  let proINDEX = Math.floor(Math.random() * pronoun.length);
+  let subINDEX = Math.floor(Math.random() * subject.length);
+  let actINDEX = Math.floor(Math.random() * action.length);
+  let posINDEX = Math.floor(Math.random() * possession.length);
+  let wheINDEX = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proINDEX] +
+    subject[subINDEX] +
+    action[actINDEX] +
+    possession[posINDEX] +
+    where[wheINDEX]
+  );
 };
